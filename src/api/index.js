f477e90944631be6575967619260b52582cf6bbf
yasirw212 <williamsyasir90@gmail.com>
2023-11-04 00:52:43 -0500

    Updating api calls

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const retrieveServices = () => axios.get('https://handy-socal-4b8bea3a3003.herokuapp.com/services')
+export const retrieveServices = () => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services')
 
-export const retrieveServicesById = (id) => axios.get('http://localhost:4001/services/:id')
+export const retrieveServicesById = (id) => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services/:id')
 
-export const retrievePopularServices = () => axios.get('https://handy-socal-4b8bea3a3003.herokuapp.com/services/popular')
+export const retrievePopularServices = () => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services/popular')
