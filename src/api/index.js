6fa983e595688f66fd9f8328dba7af058cc3589e
yasirw212 <williamsyasir90@gmail.com>
2023-11-04 19:20:48 -0500

    API calls

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-// export const retrieveServices = () => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services')
-export const retrieveServices = () => axios.get('http://localhost:4001/services')
+export const retrieveServices = () => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services')
+// export const retrieveServices = () => axios.get('http://localhost:4001/services')
 
-// export const retrieveServicesById = (id) => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services/:id')
-export const retrieveServicesById = (id) => axios.get('http://localhost:4001/services')
+export const retrieveServicesById = (id) => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services/:id')
+// export const retrieveServicesById = (id) => axios.get('http://localhost:4001/services')
 
-// export const retrievePopularServices = () => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services/popular')
-export const retrievePopularServices = () => axios.get('http://localhost:4001/services/popular')
+export const retrievePopularServices = () => axios.get('https://handysocal-backend-047adc2bc46f.herokuapp.com/services/popular')
+// export const retrievePopularServices = () => axios.get('http://localhost:4001/services/popular')
